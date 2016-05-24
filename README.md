4e1d13c348107804907e257b292f73d7525e535a
wusfen <404315887@qq.com>
2016-05-24 12:51:27 +0800

    ...

---
+++ README.md
@@ -0,0 +1,30 @@
+# clickWave.js
+
+__________________
+
+点击波浪效果插件。  
+对用户的点击交互行为，有良好的视觉反馈。
+
+__________________
+
+## USAGE 使用方法
+
+直接收入 `clickWave.js` 即可！
+
+```html
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+</head>
+
+<body>
+  在页面上任意地方点击，即可看到效果！
+  <script src="../clickWave.js"></script>
+</body>
+
+</html>
+```
+
+**[demo 查看演示](https://cdn.rawgit.com/wusfen/clickWave.js/master/example/demo.html)**
---
+++ clickWave.js
@@ -0,0 +1,57 @@
+(function () {
+  function Wave(x, y) {
+    var wave = this;
+    wave.el = document.createElement('div');
+    wave.time = 1;
+    setCss(wave.el, {
+      'pointer-events': 'none',
+      'z-index': 999999999,
+      position: 'fixed',
+      width: '100px',
+      height: '100px',
+      borderRadius: '999em',
+      background: 'rgba(111,111,111,.25)',
+      // background: 'rgba(0, 161, 255, 0.55)',
+      transform: 'translate(-50%,-50%) scale(.1)',
+      transition: wave.time + 's',
+      left: x + 'px',
+      top: y + 'px'
+    });
+  }
+  Wave.prototype={
+    show: function () {
+      document.body.appendChild(this.el);
+    },
+    remove: function () {
+      this.el.parentNode.removeChild(this.el);
+    },
+    animate: function () {
+      var that = this;
+      setTimeout(function () {
+        setCss(that.el,{
+          transform: 'translate(-50%,-50%) scale(1)',
+          opacity: '0'
+        });
+      },1);
+    }
+  };
+
+  function setCss(el, map) {
+    for (var name in map) {
+      var value = map[name];
+      el.style[name] = value;
+    }
+  }
+
+  if (new Wave().el.style.transform) {
+    document.addEventListener('click', function(e) {
+      var wave = new Wave(e.x, e.y);
+      wave.show();
+      wave.animate();
+      setTimeout(function () {
+        wave.remove();
+      }, wave.time * 1000);
+    })
+  }
+  window.Wave = Wave;
+}());
